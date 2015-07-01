a5c250a6eff6d25d984840e57dfa594f22a90158
Bernat Rafales <brafales@gmail.com>
2015-07-01 12:15:51 +0100

    first commit

---
+++ lib/telegram_bot_api.rb
@@ -0,0 +1,5 @@
+require "telegram_bot_api/version"
+
+module TelegramBotApi
+  # Your code goes here...
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,4 @@
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require 'telegram_bot_api'
+
+require 'minitest/autorun'
